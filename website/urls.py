9da6f7831556353d4e69950ca954a691a787a5d5
MaazAhmd <maazkhanahmad1@gmail.com>
2023-06-30 22:41:13 +0500

    Final Product

--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login', views.login_user, name='login'),
-    path('logout', views.logout_user, name='logout'),
-    path('register', views.register, name='register'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register, name='register'),
+    path('record/<int:pk>', views.view_record, name='view_record'),
+    path('delete/<int:pk>', views.delete_record, name='delete_record'),
+    path('add/', views.add_record, name='add_record'),
+    path('update/<int:pk>', views.update_record, name='update_record'),
 ]
--- website/views.py
+++ website/views.py
@@ -2,14 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-from .forms import SignupForm
+from .forms import SignupForm, AddRecordForm
+from .models import Record
 
 # Create your views here.
 
+
 def home(request):
+    records = Record.objects.all()
+    if not request.user.is_authenticated:
+        messages.success(request, "Please login first.")
+        return HttpResponseRedirect(reverse("login"))
     return render(request, 'website/home.html', {
-        "message" : "Login Successful"
+        "records": records
     })
 
 def login_user(request):
@@ -19,14 +26,15 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            HttpResponseRedirect(reverse('home'))
+            # HttpResponseRedirect(reverse('home'))
         else:
             return render(request, 'website/login.html', {
-                "message": "Login Failed."
+                "message": "Login Failed. Please Try Again."
             })
 
     if request.user.is_authenticated:
-        return HttpResponseRedirect(reverse('home'))
+        messages.success(request, "Login Successful.")
+        return HttpResponseRedirect(reverse("home"))
     else:
         return render(request, 'website/login.html', {})
     
@@ -43,6 +51,7 @@ def register(request):
             password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
             login(request, user)
+            messages.success(request, "welcome! Registration Successful.")
             return HttpResponseRedirect(reverse("home"))
     else:
         form = SignupForm()
@@ -51,3 +60,50 @@ def register(request):
         # })
 
     return render(request, 'website/register.html', {'form': form})
+
+def view_record(request, pk):
+    if request.user.is_authenticated:
+        record = Record.objects.get(id = pk)
+        return render(request, 'website/record.html', {
+            "record": record
+        })
+    else:
+        messages.warning(request, "You do not have the permission. Please Login First.")
+        return HttpResponseRedirect(reverse("login"))
+    
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        record = Record.objects.get(id = pk)
+        record.delete()
+        messages.success(request, "Record Deleted Successfully.")
+        return HttpResponseRedirect(reverse('home'))
+    else:
+        messages.warning(request, "You do not have the permission. Please Login First.")
+        return HttpResponseRedirect(reverse('login'))
+    
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method=='POST':
+            if form.is_valid:
+                form.save()
+                messages.success(request, "Record Added Successfully.")
+                return HttpResponseRedirect(reverse('home'))
+        return render(request, 'website/add.html', {'form': form})
+    else:
+        messages.warning(request, "You are not authorized. Please Login First.")
+        return HttpResponseRedirect(reverse('login'))
+    
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if request.method=="POST":
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Record Updated Successfully.")
+                return HttpResponseRedirect(reverse("home"))
+        return render(request, 'website/update.html', {'form': form})
+    else:
+        messages.error(request, "You must be logged in for this action.")
+        return HttpResponseRedirect(reverse("login"))
